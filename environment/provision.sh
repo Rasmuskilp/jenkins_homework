0035eee41c8ed6f3f8535e5234401da550d69cdd
Rasmus Kilp <RKilp@spartaglobal.com>
2020-01-07 09:18:49 +0000

    added new gitignore

---
+++ environment/provision.sh
@@ -0,0 +1,29 @@
+# Update the sources list
+sudo apt-get update -y
+
+# upgrade any packages available
+sudo apt-get upgrade -y
+
+
+# install git
+sudo apt-get install git -y
+sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+# install pm2
+sudo npm install pm2 -g
+
+sudo apt-get install nginx -y
+
+# remove the old file and add our one
+sudo rm /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/environment/nginx.default /etc/nginx/sites-available/default
+
+# finally, restart the nginx service so the new config takes hold
+sudo service nginx restart
+sudo apt-get install default-jre -y
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+sudo apt-get install jenkins -y
